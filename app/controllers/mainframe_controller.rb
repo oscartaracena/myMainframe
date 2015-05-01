35b014d457c7aa140b3c424679726b6f33840868
Oscar Taracena <oscar.taracena@gmail.com>
2015-04-30 17:13:35 -0700

    functional with errors

--- app/controllers/mainframe_controller.rb
+++ app/controllers/mainframe_controller.rb
@@ -1,7 +1,18 @@
 class MainframeController < ApplicationController
-  def mainframe
+  skip_before_filter  :verify_authenticity_token
+
+
+  def login
   end
 
 
 
+  def mainframe
+    @password = params['password']
+    @time = Time.now.getlocal
+    @hostname = Socket.gethostname
+    if @password != 'test'
+    redirect_to '/error' and return
+    end
+  end
 end
